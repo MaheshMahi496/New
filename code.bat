b54d0aead9ce305607e3ab8605fbb7cc002e41de
Mahesh Rasakonda <mahesh.rasakonda@int.whishworks.com>
2019-06-18 17:53:49 +0530

    Add text file

--- code.bat
+++ code.bat
@@ -2,10 +2,10 @@ cd C:\Users\mrasakonda\Desktop\Pra
 ::switch to branch you want to use
 git checkout master
 ::add all added/modified files
-git add .
+::git add .
 
 git commit -am "Add text file"
 ::push to git remote repository
-git pull --rebase
+
 git push --set-upstream "https://github.com/MaheshMahi496/NewRepo.git" master
 Pause
