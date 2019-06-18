ffe76054854b0343eaea1a8978c15b31785edfac
Mahesh Rasakonda <mahesh.rasakonda@int.whishworks.com>
2019-06-18 18:07:46 +0530

    Add new text

--- code.bat
+++ code.bat
@@ -4,7 +4,7 @@ git checkout master
 ::add all added/modified files
 ::git add .
 
-git commit -am "Add text"
+git commit -am "Add new text"
 ::push to git remote repository
 
 git push --set-upstream "https://github.com/MaheshMahi496/New.git" master
