63181a9de69ee92cdb25b182145f98f9f6ba568c
Mahesh Rasakonda <mahesh.rasakonda@int.whishworks.com>
2019-06-18 17:28:50 +0530

    deleting test2 file

---
+++ code.bat
@@ -0,0 +1,10 @@
+cd C:\Users\mrasakonda\Desktop\Pra
+::switch to branch you want to use
+git checkout master
+::add all added/modified files
+git add .
+
+git commit -am "deleting test2 file"
+::push to git remote repository
+git push --set-upstream "https://github.com/MaheshMahi496/NewRepo.git" master
+Pause
